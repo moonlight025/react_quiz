c4a433ae378f5e7e8c0da15082e56cae02445c6a
moonlight025 <tw2016905@gmail.com>
2021-04-28 16:10:23 +0800

    second

--- src/Botton.js
+++ src/Botton.js
@@ -1,11 +1,13 @@
+import Button from '@material-ui/core/Button';
 const changeText=(event)=>{
     console.log(event.target)
     event.target.innerText=event.target.innerText+"別點我"
   }
-  const multiButton=(num)=>{
-    var output=[];
-    for(let i=1;i<num+1;i++)
-        output.push(<button onClick={changeText}>第{i}個按鍵</button>)
+  const multiButton=(num)=>{  var output=[];  
+    for(let i=1;i<num+1;++i) 
+    {output.push(<Button variant="contained" color="primary"   
+    onClick={changeText}>
+        第{i}個按鍵</Button>)}  
     return output;
-  }
+}
   export default multiButton;
